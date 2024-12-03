e2f8b8e1b7ac4a6104ecd462101f4e9720b8777e
hjortr <rutrojh@gmail.com>
2024-12-03 11:23:35 +0000

    Updated staff.py

--- Model/Staff.py
+++ Model/Staff.py
@@ -1,7 +1,7 @@
 
 
 class Staff:
-    def __init__(self,name,ssn,address,home_phone,mobile_phone,email,location):
+    def __init__(self,name,ssn,address,home_phone,mobile_phone,email,location,username,password):
         self.name = name
         self.ssn = ssn
         self.address = address
@@ -9,5 +9,7 @@ class Staff:
         self.mobile_phone = mobile_phone
         self.email = email
         self.location = location
+        self.username = username
+        self.password = password
 
 #TODO klára fallið
