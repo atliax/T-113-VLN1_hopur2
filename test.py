f9de3161f3ef7b398531819657656e31a74ae466
atliax <atliax@gmail.com>
2024-12-05 11:20:39 +0000

    Fjarlægði prufuskrá.

--- test.py
+++
@@ -1,8 +0,0 @@
-from StorageLayer.property_storage import PropertyStorage
-from Model import Property
-
-test = PropertyStorage('StorageLayer/test.json')
-
-test2 = test.load_from_file(Property)
-
-print(test2)
