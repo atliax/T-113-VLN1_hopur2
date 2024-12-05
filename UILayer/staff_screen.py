2127535c645c6a782cee63339d4bff1aacc9eeec
hjortr <rutrojh@gmail.com>
2024-12-05 10:42:20 +0000

    Ifs and buts. Few screens also created
    Went through the happy path and added that to each screen.

--- UILayer/staff_screen.py
+++ UILayer/staff_screen.py
@@ -1,4 +1,5 @@
 from UILayer.base_screen import BaseScreen
+from UILayer import ui_consts
 
 class StaffScreen(BaseScreen):
     def __init__(self, ui):
@@ -7,8 +8,48 @@ class StaffScreen(BaseScreen):
     def render(self):
         self.clear_screen()
 
-        print("Staff")
+        print("Main menu > Staff")
+        print(ui_consts.SEPERATOR)
+        print("|")
+        print("|	[A] Add an employee		    [E] Edit a destination			[B] Go back")
+        print("|	[R] Remove an employee      [S] Search for")
+        print("|	[C] View contractors")
+        print("|")
+        print(ui_consts.SEPERATOR)
 
         cmd = input("Command: ")
+
+        if cmd == "a":
+            new_employee = input("New employee name: ")
+            new_phone_nr = input("New employee phone number: ")
+            new_email = input("New employee email: ")
+            new_ssn = input("New employee ssn: ")
+            new_title = input("New employee job title: ")
+            new_location = input("New employee location: ")
+            # Starfsmaður er bættur við á listann. Auðkenni er búið til sjálfkrafa af forritinu 
+
+        if cmd == "r":
+            remove_employee = input("Remove employee with the ID: ")
+
+        if cmd == "e":
+            #if nothing is input, the field will be left unchanged
+            change_name = input("Change employey name to: ")
+            change_number = input("Change employee phone number to: ")
+            change_email = input("Change employee email to: ")
+            change_title = input("Change employee job title to: ")
+            change_location = input("Change employee location: ")
+
+        if cmd == "s":
+            # Example gamer, Nuuk
+            # Finnur allar línur tengdar gamer, nuuk
+            search = input("Search for: ")
+
+        if cmd == "c":
+            # return contractor screen
+            pass
+
+        if cmd == "b":
+            return ui_consts.BACK
+
 
         return self
--- UILayer/tickets_screen.py
+++ UILayer/tickets_screen.py
@@ -1,4 +1,5 @@
 from UILayer.base_screen import BaseScreen
+from UILayer import ui_consts
 
 class TicketsScreen(BaseScreen):
     def __init__(self, ui):
@@ -7,8 +8,51 @@ class TicketsScreen(BaseScreen):
     def render(self):
         self.clear_screen()
 
-        print("Tickets")
+        print("Main menu > Tickets")
+        print(ui_consts.SEPERATOR)
+        print("|")
+        print("|	[A] Add a ticket		    [E] Edit a ticket			[B] Go back")
+        print("|	[R] Remove a ticket	        [S] Search for")
+        print("|	[V] View closed tickets     [RV] Review a ticket report")
+        print("|	[D] Ticket details")
+        print("|")
+        print(ui_consts.SEPERATOR)
 
         cmd = input("Command: ")
+
+        # Add a ticket
+        if cmd == "a":
+            # Go to "Main menu > Tickets > Ticket maker"
+            pass
+
+        # Remove a ticket
+        if cmd == "r":
+            remove_ticket = input("Remove ticket with ID: ")
+
+        # Edit ticket
+        if cmd == "e":
+            edit_ticket = input("Edit ticket with the ID: ")
+            # Go to "Main menu > Tickets > Ticket editor"
+
+        # search
+        if cmd == "s":
+            search = input("Search for: ")
+
+        # View closed tickets
+        if cmd == "v":
+            # go to submenu "Main menu > Tickets > Closed tickets"
+            pass
+
+        # Review
+        if cmd == "rv":
+            review = input("Review ticket with ID: ")
+        # go to submenu "Main menu > Tickets > Review report ticket: (TICKET ID)"
+
+        if cmd == "d":
+            details = input("View ticket with ID: ")
+        # Go to "Main menu > Tickets > Ticket details"
+
+        if cmd == "b":
+            return ui_consts.BACK
 
         return self
