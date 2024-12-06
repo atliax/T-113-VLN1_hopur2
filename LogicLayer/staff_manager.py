c29e8443c946121c3c7ef932469c7c4d414ecc38
atliax <atliax@gmail.com>
2024-12-06 11:26:54 +0000

    lagaði crash vegna breytinga á model klasa

--- LogicLayer/staff_manager.py
+++ LogicLayer/staff_manager.py
@@ -9,15 +9,15 @@ class StaffManager:
 
     def authenticate_login(self,email,password):
         if email.upper() == 'BOSS' and password == 'Man':
-            self.logged_in_staff = Staff("1","Dóttir Sigga Ísmanns","090488-2959","Grundargata 12", "555-5555", "867-5309", "siggi@nanair.is", "flubber", None,"Yfirmaður rekstrarsviðs",True)
-            self.tmp_destination = Destination("A1","Ísland","Keflavík","555-5556","24/7",self.logged_in_staff.id)
-            self.logged_in_staff.destinationID = self.tmp_destination.id
+            self.logged_in_staff = Staff("1","A1","Dóttir Sigga Ísmanns","090488-2959","Grundargata 12", "555-5555", "867-5309", "siggi@nanair.is", "flubber","Yfirmaður rekstrarsviðs",True)
+            self.tmp_destination = Destination("A1","Ísland","Keflavík","555-5556","24/7",self.logged_in_staff.staffID)
+            self.logged_in_staff.destinationID = self.tmp_destination.destinationID
             return True
 
         if email.upper() == 'PLEB' and password == '1234':
-            self.logged_in_staff = Staff("2","Jón afi","120488-2959","Grundargata 14", "555-5555", "867-5309", "siggi@nanair.is", "flubber", None,"Yfirmaður rekstrarsviðs",False)
-            self.tmp_destination = Destination("A1","Ísland","Keflavík","555-5556","24/7",self.logged_in_staff.id)
-            self.logged_in_staff.destinationID = self.tmp_destination.id
+            self.logged_in_staff = Staff("2","A1","Jón afi","120488-2959","Grundargata 14", "555-5555", "867-5309", "siggi@nanair.is", "flubber","Yfirmaður rekstrarsviðs",False)
+            self.tmp_destination = Destination("A1","Ísland","Keflavík","555-5556","24/7",self.logged_in_staff.staffID)
+            self.logged_in_staff.destinationID = self.tmp_destination.destinationID
             return True
 
         return False
