ebdd5ec1ebe171efb746ac6748a833646ff78289
hjortr <rutrojh@gmail.com>
2024-12-06 13:24:48 +0000

    testadata added

--- StorageLayer/storage_api.py
+++ StorageLayer/storage_api.py
@@ -10,13 +10,13 @@ from Model import *
 
 class StorageAPI:
     def __init__(self):
-        self.facility_storage = FacilityStorage("filename.json", Facility)
-        self.contractor_storage = ContractorStorage("filename.json", Contractor)
+        self.facility_storage = FacilityStorage("testdata/facilities.json", Facility)
+        self.contractor_storage = ContractorStorage("testdata/contractors.json", Contractor)
         self.destination_storage = DestinationStorage("testdata/destinations.json", Destination)
         self.property_storage = PropertyStorage("testdata/properties.json", Property)
-        self.report_storage = ReportStorage("filename.json", Report)
-        self.staff_storage = StaffStorage("filename.json", Staff)
-        self.ticket_storage = TicketStorage("filename.json", Ticket)
+        self.report_storage = ReportStorage("testdata/reports.json", Report)
+        self.staff_storage = StaffStorage("testdata/staff.json", Staff)
+        self.ticket_storage = TicketStorage("testdata/tickets.json", Ticket)
 
     def get_all_properties(self) -> list[Property]:
         return self.property_storage.load_from_file()
