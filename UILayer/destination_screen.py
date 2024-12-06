d9dead0da3241c97e86b1b30f86b4ba3a8b8806c
hjortr <rutrojh@gmail.com>
2024-12-06 12:19:44 +0000

    updated screen according to happy path

--- UILayer/destination_screen.py
+++ UILayer/destination_screen.py
@@ -26,7 +26,6 @@ class DestinationScreen(BaseScreen):
         print(ui_consts.SEPERATOR)
         print("|")
         print("|	[A] Add a destination		[E] Edit a destination			[B] Go back")
-        print("|	[R] Remove a destination	[S] Search for")
         print("|")
         print(ui_consts.SEPERATOR)
 
@@ -34,23 +33,23 @@ class DestinationScreen(BaseScreen):
 
         # Add a destination
         if cmd == "a":
-            new_dest = input("New destination: ")
+            new_destination = input("New destination country: ")
             new_airport = input("New destination airport: ")
             new_opening_hours = input("New destination airport opening hours: ")
-
-        # Remove destination
-        if cmd == "r":
-            remove = input("Remove destination with ID: ")
+            new_phone = int(input("New destination phone number: "))
+            new_manager = input("New destination manager: ")
 
         # Edit destination
         if cmd == "e":
-            #if nothing is input, the name/loc will be unchanged
-            change_dest = input("Change destination: ")
-            change_airport = input("Change destination airport: ")
-            change_opening_hours = input("Change destination airport opening hours: ")
-
-        if cmd == "s":
-            search = input("Search for: ")
+            edit_id = input("Edit destination with the ID: ")
+                # If ID does not exist in the destination list, raise error "No destination found with that ID!"    
+                 # If ID does not exist, cancel command
+            print("If you do not wish to change a specific field, you can leave the input empty")
+            change_destination = input("Change destination country to: ")
+            change_airport = input("Change destination airport to: ")
+            change_opening_hours = input("Change destination airport opening hours to: ")
+            change_number = int(input("Change destination phone number to: "))
+            change_manager = input("Change desination manager to: ")
         
         if cmd == "b":
             return ui_consts.BACK
