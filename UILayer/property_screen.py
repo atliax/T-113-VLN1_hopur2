83cafa77300591daac3ea0b7365914c0b9bad445
atliax <atliax@gmail.com>
2024-12-09 23:52:52 +0000

    Smá manual merge vegna leiðréttingar

--- UILayer/property_screen.py
+++ UILayer/property_screen.py
@@ -43,14 +43,14 @@ class PropertyScreen(BaseScreen):
         if cmd == "a":
             # Get property detail inputs from user 
             p_new_name = input("New property name: ")
-            p_new_location = input("New property destionation ID: ")
+            p_new_destination = input("New property destionation ID: ")
             p_new_address = input("New property address: ")
-            p_new_square = int(input("New property square meters: "))
+            p_new_square_mtrs = int(input("New property square meters: "))
             p_new_roomnum = int(input("New property number of rooms: "))
             p_new_type = input("New property type: ")
 
             # construct property
-            new_property = Property(None, p_new_name, p_new_location, p_new_address, p_new_square, p_new_roomnum, p_new_type)
+            new_property = Property(None, p_new_name, p_new_destination, p_new_address, p_new_square_mtrs, p_new_roomnum, p_new_type)
             
             # Send property to logic api
             self.ui.logic_api.property_add(new_property)
@@ -58,7 +58,8 @@ class PropertyScreen(BaseScreen):
 
         # [R] Remove a property
         if cmd == "r":
-            remove_property = input("Remove a property that has the ID: ")
+            remove_id = input("Remove a property that has the ID: ").upper()
+            self.ui.logic_api.property_remove(remove_id)
 
         # [V] View facilities
         if cmd == "v":
@@ -67,23 +68,23 @@ class PropertyScreen(BaseScreen):
 
         # [E] Edit a property 
         if cmd == "e":
-            edit_property = input("Edit property with the ID: ") # If nothing is input, the field will be left unchanged
+            p_edit_property = input("Edit property with the ID: ") # If nothing is input, the field will be left unchanged
                 # If ID does not exist in property list, raise error "No property found with that ID!"
                 # If ID does not exist, cancel command EKKI IMPLEMENTAÐ
             print("If you do not wish to change a specific field, you can leave the input empty")
-            change_name = input("Change property name to: ")
-            change_destination = input("Change property destination to: ")
-            change_address = input("Change property address to: ")
-            change_square_mtrs = int(input("Change property square meters to: "))
-            change_rooms = int(input("Change property number of rooms to: "))
-            change_property_type = input("Change property type to: ")
+            p_change_name = input("Change property name to: ")
+            p_change_destination = input("Change property destination to: ")
+            p_change_address = input("Change property address to: ")
+            p_change_square_mtrs = int(input("Change property square meters to: "))
+            p_change_rooms = int(input("Change property number of rooms to: "))
+            p_change_property_type = input("Change property type to: ")
 
         # [S] Search for
         if cmd == "s":
             print("What keyword would you like to search for?")
             print("You can combine keywords by following a word with ,")
             print("Example: (Grænland,Nuukstræti 4)")
-            search = input("Search for: ")       # Sama search fyrir alla skjái
+            p_search = input("Search for: ")       # Sama search fyrir alla skjái
                 # print out a new filtered list based on keywords input
                 # GoTo "Main menu > Properties > Filtered"
                 # "Main menu > Properties > Filtered" window and commands are identical to "Main menu > Properties"
