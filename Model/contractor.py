50c071d0ed7ac34c1e27a1248e80cb84dd9403d0
Dnl-CS <119466749+Dnl-CS@users.noreply.github.com>
2024-12-09 13:31:46 +0000

    Update contractor.py

--- Model/contractor.py
+++ Model/contractor.py
@@ -3,14 +3,12 @@ from Model.base_model import BaseModel
 class Contractor(BaseModel):
     def __init__(self, contractorID:str, destinationID:str, rating:float, name:str, contact:str, phone:str, address:str, opening_hours:str, contractor_type:str) -> None:
         self.contractorID = contractorID
-        self.name = name
-        self.contractor_type = contractor_type        
         self.destinationID = destinationID
-        self.contact = contact
         self.rating = rating
+        self.name = name
         self.contact = contact
         self.phone = phone
         self.address = address
         self.opening_hours = opening_hours
-
+        self.contractor_type = contractor_type
 
