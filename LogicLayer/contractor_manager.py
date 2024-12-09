104ecb3f67f90192d2c6052c984da4117c15270d
Dnl-CS <119466749+Dnl-CS@users.noreply.github.com>
2024-12-09 09:58:12 +0000

    Contractor fluff

--- LogicLayer/contractor_manager.py
+++ LogicLayer/contractor_manager.py
@@ -1,3 +1,34 @@
+from Model import Contractor
+
 class ContractorManager:
     def __init__(self, storage_api):
         self.storage_api = storage_api
+
+    def get_all_contractors(self):
+        return self.storage_api.get_all_contractors()
+    
+    def get_logged_in_contractor(self) -> Contractor:
+        return self.logged_in_contractor
+
+    def logout(self):
+        self.logged_in_contractor = None
+        pass
+
+    def add_new_contractor(self, new_contractor : Contractor):
+        all_contractor = self.storage_api.get_all_contractors()
+        n = int(all_contractor[len(all_contractor)-1].contractorID[1:])
+        n += 1
+        new_id = "S" + str(n)
+        new_contractor.contractorID = new_id
+
+
+        self.storage_api.add_new_contractor(new_contractor)
+
+    def remove_contractor(self):
+        pass
+
+    def edit_contractor(self):
+        pass
+
+    def search_contractor(self):
+        pass
--- StorageLayer/contractor_storage.py
+++ StorageLayer/contractor_storage.py
@@ -1,5 +1,12 @@
 from StorageLayer.base_storage import BaseStorage
 
+from Model import Contractor
+
 class ContractorStorage(BaseStorage):
     def __init__(self, filename, model_class):
         super().__init__(filename, model_class)
+    
+    def add_new_contractor(self, new_contractor : Contractor):
+        current_contractor = self.load_from_file()
+        current_contractor.append(Contractor)
+        self.save_to_file(current_contractor)
