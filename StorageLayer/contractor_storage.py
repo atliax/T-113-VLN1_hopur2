d6a1bce87b2a02595d1953b6512f984dd6f7be5e
Dnl-CS <119466749+Dnl-CS@users.noreply.github.com>
2024-12-09 11:56:09 +0000

    remove contractor

--- StorageLayer/contractor_storage.py
+++ StorageLayer/contractor_storage.py
@@ -10,7 +10,11 @@ class ContractorStorage(BaseStorage):
         current_contractor.append(new_contractor)
         self.save_to_file(current_contractor)
 
-    def remove_contractor(self, current_contractor : Contractor):
-        current_contractor = self.load_from_file()
-        current_contractor.remove(current_contractor)
+    def remove_contractor(self, remove_id: str):
+        current_contractor: list[Contractor] = self.load_from_file()
+        new_contractor_list = []
+        for contractor in current_contractor:
+            if contractor.contractorID != remove_id:
+                new_contractor_list.append(contractor)
+        self.save_to_file(new_contractor_list)
         
