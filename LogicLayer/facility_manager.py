88f0fb527a3f89cb2b77d8d76bef32ce80638fca
evadisndat <evah23@ru.is>
2024-12-10 09:58:27 +0000

    updated facility, þarf að laga a

--- LogicLayer/facility_manager.py
+++ LogicLayer/facility_manager.py
@@ -6,33 +6,12 @@ class FacilityManager:
     def __init__(self, storage_api : StorageAPI) -> None:
         self.storage_api = storage_api
 
-    def facility_add(self, new_facility : Facility) -> None:
-        all_facilities = self.storage_api.facility_get_all()
-        n = int(all_facilities[len(all_facilities)-1].facilityID[1:])
-        n += 1
-        new_id = "F" + str(n)
-        new_facility.facilityID = new_id
 
-        self.storage_api.facility_add(new_facility)
-
-    def facility_remove(self, facilityID : str) -> None:
-        # TODO validation
-
-        # ef í lagi:
-        self.storage_api.facility_remove(facilityID)
-
-    def facility_edit():
-        pass
-
+    # Birtir lista af facilities     
     def facility_get_all(self) -> list[Facility]:
         return self.storage_api.facility_get_all()
-
-    def facility_get_by_ID(self, facilityID : str) -> Facility:
-        return self.storage_api.facility_get_by_ID(facilityID)
-
-    def facility_search(self, search_string : str):
-        return self.storage_api.facility_search(search_string)
-
+    
+    # Leitar að facility eftir innslegnu property ID
     def facility_get_by_propertyID(self, propertyID : str) -> list[Facility]:
         all_facilities = self.storage_api.facility_get_all()
 
@@ -43,3 +22,37 @@ class FacilityManager:
                 filtered_facilities.append(facility)
 
         return filtered_facilities
+
+
+    # [A] to add a facility
+    def facility_add(self, new_facility : Facility) -> None:
+        all_facilities = self.storage_api.facility_get_all()
+        n = int(all_facilities[len(all_facilities)-1].facilityID[1:])
+        n += 1
+        new_id = "F" + str(n)
+        new_facility.facilityID = new_id
+
+        self.storage_api.facility_add(new_facility)
+
+    # [R] to remove a facility
+    def facility_remove(self, facilityID : str) -> None:
+        # TODO validation
+
+        # ef í lagi:
+        self.storage_api.facility_remove(facilityID)
+
+
+    # [V] to view details of facility with property ID 
+    def facility_get_by_ID(self, facilityID : str) -> Facility:
+        return self.storage_api.facility_get_by_ID(facilityID)
+    
+
+    # [S] to search for a facility
+    def facility_search(self, search_string : str):
+        return self.storage_api.facility_search(search_string)
+
+    # [E] to edit a facility
+    def facility_edit(self, edited_facility : Facility):
+        self.storage_api.facility_edit(edited_facility)
+
+
