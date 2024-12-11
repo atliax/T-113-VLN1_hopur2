c2efcb15cb49c27a90045cc7ac85cdcd0e5d18a9
atliax <atliax@gmail.com>
2024-12-11 13:32:06 +0000

    Setti auto ID á report_add

--- LogicLayer/report_manager.py
+++ LogicLayer/report_manager.py
@@ -13,6 +13,12 @@ class ReportManager:
         return self.storage_api.report_get_by_ID(reportID)
 
     def report_add(self, new_report : Report) -> None:
+        all_reports = self.storage_api.report_get_all()
+        n = int(all_reports[len(all_reports)-1].ID[1:])
+        n += 1
+        new_id = "R" + str(n)
+        new_report.ID = new_id
+
         self.storage_api.report_add(new_report)
 
     def report_edit(self, edited_report : Report) -> None:
