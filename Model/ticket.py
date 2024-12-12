08c2948161fdfafc9dfafa66904dd8b935ceadef
atliax <atliax@gmail.com>
2024-12-12 08:54:24 +0000

    bætti close date á ticket model

--- Model/ticket.py
+++ Model/ticket.py
@@ -4,7 +4,7 @@ class Ticket(BaseModel):
     def __init__(self, ID : str, facilityID : str,
                  propertyID : str, priority : str, title : str, description : str, \
                  open : bool, status : str, recurring : bool, recurring_days : int, \
-                 open_date : str, staffID : str, report : str, cost : int, 
+                 open_date : str, close_date : str, staffID : str, report : str, cost : int, 
                  contractorID : str, contractor_review : str, contractor_rating : float,
                  contractor_fee : int) -> None:
         self.ID = ID
@@ -18,6 +18,7 @@ class Ticket(BaseModel):
         self.recurring = recurring
         self.recurring_days = recurring_days
         self.open_date = open_date
+        self.close_date = close_date
         self.staffID = staffID
         self.report = report
         self.cost = cost
