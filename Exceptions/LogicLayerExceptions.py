195e3ffb2d9d7b74786e60da9e66539ca4c6e166
atliax <atliax@gmail.com>
2024-12-12 21:41:37 +0000

    docstrings í Exceptions

--- Exceptions/LogicLayerExceptions.py
+++ Exceptions/LogicLayerExceptions.py
@@ -2,5 +2,6 @@ class LogicLayerException(Exception):
     pass
 
 class IDNotFoundError(LogicLayerException):
+    """Raised when a requested Model instance does not exist in the system."""
     def __init__(self, message) -> None:
         super().__init__(message)
--- Exceptions/StorageLayerExceptions.py
+++ Exceptions/StorageLayerExceptions.py
@@ -2,9 +2,11 @@ class StorageLayerException(Exception):
     pass
 
 class FileWriteError(StorageLayerException):
+    """Raised when there are errors during file writing."""
     def __init__(self, message) -> None:
         super().__init__(message)
 
 class FileReadError(StorageLayerException):
+    """Raised when there are errors during file reading."""
     def __init__(self, message) -> None:
         super().__init__(message)
